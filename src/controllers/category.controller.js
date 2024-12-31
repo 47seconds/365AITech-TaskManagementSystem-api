1f8874c1f779e1fa13cbb5f3c28238efa3d71ded
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-12-31 20:24:36 +0530

    created add category logic

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -4,8 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.util.js";
 import { Category } from "../models/category.model.js";
 
 export const getUserCategories = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const categories = await Category.find({ user: userId });
+  const categories = await Category.find({ user: req.user._id });
   if (!categories) throw new ApiError(404, "no categories found for this user");
   return res
     .status(200)
@@ -16,5 +15,23 @@ export const getUserCategories = asyncHandler(async (req, res) => {
         req.user,
         "user categories successfully fetched"
       )
+    );
+});
+
+export const addUserCategory = asyncHandler(async (req, res) => {
+  const {name} = req.body;
+  if (!name) throw new ApiError(400, "category name not parsed");
+  const category = Category.create(
+    {
+      user: req.user._id,
+      name: name
+    }
+  );
+
+  if (!category) throw new ApiError(500, "unable to add given category in database");
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, category._doc, req.user, "successfully added category in database")
     );
 });
