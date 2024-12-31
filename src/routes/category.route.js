af97cbc153b486aeaa34bdd3ac89f9d9b9315c57
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-12-31 20:22:55 +0530

    renamed file and added new routes

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getUserCategories } from "../controllers/category.controller.js";
+import { getUserCategories, addUserCategory } from "../controllers/category.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { userSession } from "../middlewares/userSession.middleware.js";
 
 export const categoryRouter = Router();
 
 categoryRouter.route("/").get(verifyJWT, getUserCategories);
+categoryRouter.route("/").post(verifyJWT, addUserCategory);
