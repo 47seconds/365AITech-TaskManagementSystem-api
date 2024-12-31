8c833ecda449a900d41b8f03259176aa13dbf8bc
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-12-31 23:43:02 +0530

    implemented tsk delete logic

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -19,19 +19,42 @@ export const getUserCategories = asyncHandler(async (req, res) => {
 });
 
 export const addUserCategory = asyncHandler(async (req, res) => {
-  const {name} = req.body;
+  const { name } = req.body;
   if (!name) throw new ApiError(400, "category name not parsed");
-  const category = Category.create(
-    {
-      user: req.user._id,
-      name: name
-    }
-  );
+  const category = Category.create({
+    user: req.user._id,
+    name: name,
+  });
 
-  if (!category) throw new ApiError(500, "unable to add given category in database");
+  if (!category)
+    throw new ApiError(500, "unable to add given category in database");
   return res
-  .status(200)
-  .json(
-      new ApiResponse(200, category._doc, req.user, "successfully added category in database")
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        category._doc,
+        req.user,
+        "successfully added category in database"
+      )
+    );
+});
+
+export const deleteUserCategory = asyncHandler(async (req, res) => {
+  const { category_id } = req.params;
+  if (!category_id) throw new ApiError(404, "no category was provided");
+  const deleteCategory = await Category.findByIdAndDelete(category_id);
+  if (deleteCategory == null)
+    throw new ApiError(500, "failed to delete category");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        deleteCategory,
+        req.user,
+        "category deleted successfully"
+      )
     );
 });
