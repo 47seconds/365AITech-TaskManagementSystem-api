1927d677ed1df3832bf7f5cf70296c56f0a3c2aa
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-12-31 16:49:28 +0530

    added utilities funtions

---
+++ src/utils/ApiResponse.util.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+  constructor(statusCode, data, user, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+    this.user = user;
+  }
+}
+
+export { ApiResponse };
---
+++ src/utils/asyncHandler.util.js
@@ -0,0 +1,33 @@
+import { ApiError } from "./ApiError.util.js";
+
+const asyncHandler = (reqHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(reqHandler(req, res, next)).catch((error) => {
+      if (error instanceof ApiError) {
+        return res.status(error.statusCode || 500).json({
+          success: error.success,
+          statusCode: error.statusCode,
+          errors: error.errors,
+          stack: error.stack.trim(),
+          message: error.message,
+        });
+      } else {
+        const otherError = new ApiError(
+          500,
+          error.message,
+          error.name,
+          error.stack
+        );
+        return res.status(otherError.statusCode).json({
+          success: otherError.success,
+          statusCode: otherError.statusCode,
+          error: otherError.name,
+          stack: otherError.stack.trim(),
+          message: otherError.message,
+        });
+      }
+    });
+  };
+};
+
+export { asyncHandler };
